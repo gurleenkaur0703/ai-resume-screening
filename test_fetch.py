14dd6efe4b41178e3789848dcf4e28fed8543a94
gurleenkaur0703 <gurleenkaur.gk03@gmail.com>
2025-12-22 21:15:48 +0530

    Ignore test files and local artifacts

--- test_fetch.py
+++
@@ -1,4 +0,0 @@
-from utils.db_utils import fetch_all_resumes_with_skills
-
-data = fetch_all_resumes_with_skills()
-print(data)
--- test_pdf.py
+++
@@ -1,7 +0,0 @@
-from utils.pdf_parser import extract_text_from_pdf
-
-with open("sample_resume.pdf", "rb") as f:
-    text = extract_text_from_pdf(f)
-
-print("Extracted text preview:\n")
-print(text[:1000])
